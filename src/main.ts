f0bac51d70d4dd0b095a6368548263c7fd5e9cb3
anhvuDev103 <anhvu.dev.103@gmail.com>
2026-07-08 15:27:30 +0700

    disable swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { cleanupOpenApiDoc } from 'nestjs-zod';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -12,14 +10,14 @@ async function bootstrap() {
   //   prefix: '/media/static',
   // });
 
-  const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
-    .setVersion('1.0')
-    .addTag('cats')
-    .build();
-  const documentFactory = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, cleanupOpenApiDoc(documentFactory));
+  // const config = new DocumentBuilder()
+  //   .setTitle('Cats example')
+  //   .setDescription('The cats API description')
+  //   .setVersion('1.0')
+  //   .addTag('cats')
+  //   .build();
+  // const documentFactory = SwaggerModule.createDocument(app, config);
+  // SwaggerModule.setup('api', app, cleanupOpenApiDoc(documentFactory));
 
   await app.listen(process.env.PORT ?? 3000);
 }
